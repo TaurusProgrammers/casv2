d16354bc7833b13e4eca8b8f7e4444840e899c51
Kevin <kevinmolinataylor@gmail.com>
2018-10-24 14:00:17 -0500

    Message and comprobation conection

--- php/conexion.php
+++ php/conexion.php
@@ -32,4 +32,13 @@
 	}
 
 	$conexion = conectarse();
+	if (!$conexion) {
+    echo "Error: No se pudo conectar a MySQL." . PHP_EOL;
+    echo "errno de depuración: " . mysqli_connect_errno() . PHP_EOL;
+    echo "error de depuración: " . mysqli_connect_error() . PHP_EOL;
+    exit;
+	}
+	echo "Éxito: Se realizó una conexión apropiada a MySQL!" .PHP_EO;
+	echo "Información del host: " . mysqli_get_host_info($conexion) . PHP_EOL;
+	mysqli_close($conexion);
 ?>
